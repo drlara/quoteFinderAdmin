1d7d3b3faaafc4245019e0da202b47b27a1dab74
mist-miguel-lara <123275349+mist-miguel-lara@users.noreply.github.com>
2025-11-04 17:37:13 -0800

    adding first and last names to database

---
+++ index.mjs
@@ -0,0 +1,57 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//for Express to get values using POST method
+app.use(express.urlencoded({extended:true}));
+
+//setting up database connection pool
+const pool = mysql.createPool({
+    host: "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "w9c7lwn8um1o99yj",
+    password: "u3rw8lbcasz2h307",
+    database: "pyn5h5u7iu857dd2",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+
+//routes
+app.get('/', (req, res) => {
+   res.render('home.ejs')
+});
+
+//Displays form to add a new author
+app.get('/addAuthor', (req, res) => {
+   res.render('addAuthor.ejs')
+});
+
+//Stores author data into the database
+app.post('/addAuthor', async (req, res) => {
+    let firstName = req.body.fn;
+    let lastName = req.body.ln;
+    let sql = `INSERT INTO authors
+                (firstName, lastName)
+                VALUES (?, ?)`;
+    let sqlParams = [firstName, lastName];  
+    const [rows] = await pool.query(sql, sqlParams);       
+    res.render('addAuthor.ejs')
+});
+
+app.get("/dbTest", async(req, res) => {
+   try {
+        const [rows] = await pool.query("SELECT CURDATE()");
+        res.send(rows);
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error!");
+    }
+});//dbTest
+
+app.listen(3000, ()=>{
+    console.log("Express server running")
+})
+
